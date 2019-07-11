93b728ac9c9d48ab20466ae1901d80512c2b503c
Kingiz1ch <alplott@gmail.com>
2019-07-11 19:11:18 +0300

    slider ready

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+		loop:true,
+    margin:10,
+		nav:true,
+		dots:true,
+		dotsEach:true,
+		items:1
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:1
+    //     },
+    //     1000:{
+    //         items:1
+    //     }
+    // }
+	});
+});
