dfc15770d49ffc8a5301afd97d7e28cea7971a6e
Рудольф <Stiazz96@yandex.ru>
2019-07-26 00:33:53 +0500

    navbar and banner

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,6 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        dots: true,
-        dotsEach: true,
-        items: 1,
-        video:true
-    // responsive:{
-    //     0:{
-    //         items:1
-    //     },
-    //     600:{
-    //         items:1
-    //     },
-    //     1000:{
-    //         items:1
-    //     }
-    // }
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".navbar.fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
