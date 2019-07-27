5141dda1fbc61e0356309793adc015a29c0c5c3a
Рудольф <Stiazz96@yandex.ru>
2019-07-27 22:49:38 +0500

    js get autist

--- js/custom.js
+++ js/custom.js
@@ -4,16 +4,3 @@ $(function() {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
-$(function() {
-    $.getJSON('../gameList.json', function(data) {
-        alert(data);
-        if (data) {
-            alert("OK");
-            for (var i = 0; i < data.length; i++) {
-                $('#table').append('<tr><td>' + data[i].steamId + '</td><td>' + data[i].gameName +
-                    '</td><td>' + data[i].gameRate + '</td><tr>');
-            }
-        } else alert("ERROR");
-
-    });
-});
--- js/printList.js
+++ js/printList.js
@@ -1,37 +1,54 @@
 var count_show = parseInt($('#show_more').attr('count_show'));
 var count_add = parseInt($('#show_more').attr('count_add'));
+$(document).ready(function() {
+    $.ajaxSetup({ cache: false });
+});
 $(function() {
-
-    $.getJSON("/gameLoad.php?link=http://store.steampowered.com/api/appdetails?appids=57690&l=ru", function(data) {
-        console.log(data);
+    $.getJSON("/gameList.json", function(dataApp) {
+        console.log(dataApp[0].steamId);
+        $.getJSON('/gameLoad.php?link=http://store.steampowered.com/api/appdetails?appids=' + dataApp[0].steamId + '&l=ru', function(dataApi) {
+            var key1 = "data";
+            console.log(dataApi[dataApp[0].steamId][key1].header_image);
+        });
     });
-
-    $.getJSON("/js/gameList.json", function(data) {
-        for (var i = count_show; i < count_show + count_add; i++) {
-            if (data[i].steamId != null) {
-                $('#gameList').append(
-                    '<div class="col-12 col-md-6 col-xl-4">' +
-                    '<div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
-                    '<div class="Portfolio"><a href="https://store.steampowered.com/app/' + data[i].steamId + '/">' +
-                    '<img class="card-img" src="https://steamcdn-a.akamaihd.net/steam/apps/' + data[i].steamId + '/header.jpg">' +
-                    '</a><div class="desc">' + data[i].gameName +
-                    '</div>' +
-                    '</div>'
-                )
-            } else {
-                $('#gameList').append(
-                    '<div class="col-12 col-md-6 col-xl-4">' +
-                    '<div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
-                    '<div class="Portfolio"><a href="' + data[i].url + '/">' +
-                    '<img class="card-img" src="' + data[i].headerImgURL + '">' +
-                    '</a><div class="desc">' + data[i].gameName +
-                    '</div>' +
-                    '</div>'
-                )
-            }
-        }
-        count_show += count_add;
-    });
+    var keyData = "data";
+    // $.getJSON("/gameList.json", function(dataApp) {
+    //     for (var i = count_show; i < count_show + count_add; i++) {
+    //         if (dataApp[i].steamId != null) {
+    //             $.getJSON("/gameLoad.php?link=http://store.steampowered.com/api/appdetails?appids=" + dataApp[i].steamId + "&l=ru", function(dataApi) {
+    //                 $('#gameList').append(
+    //                     '<div class="col-12 col-md-6 col-xl-4">' +
+    //                     '<div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
+    //                     '<div class="Portfolio"><a href="https://store.steampowered.com/app/' + dataApp[i].steamId + '/">' +
+    //                     '<img class="card-img" src="' + dataApi[dataApp[i].steamId][keyData].header_image + '">' +
+    //                     '</a><div class="desc">' + dataApi[dataApp[i].steamId][keyData].name +
+    //                     '</div>' +
+    //                     '</div>'
+    //                 )
+    //             });
+    //             // $('#gameList').append(
+    //             //     '<div class="col-12 col-md-6 col-xl-4">' +
+    //             //     '<div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
+    //             //     '<div class="Portfolio"><a href="https://store.steampowered.com/app/' + data[i].steamId + '/">' +
+    //             //     '<img class="card-img" src="https://steamcdn-a.akamaihd.net/steam/apps/' + data[i].steamId + '/header.jpg">' +
+    //             //     '</a><div class="desc">' + data[i].gameName +
+    //             //     '</div>' +
+    //             //     '</div>'
+    //             // )
+    //         } else {
+    //             $('#gameList').append(
+    //                 '<div class="col-12 col-md-6 col-xl-4">' +
+    //                 '<div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
+    //                 '<div class="Portfolio"><a href="' + dataApp[i].url + '/">' +
+    //                 '<img class="card-img" src="' + dataApp[i].headerImgURL + '">' +
+    //                 '</a><div class="desc">' + dataApp[i].gameName +
+    //                 '</div>' +
+    //                 '</div>'
+    //             );
+    //         }
+    //     }
+    //     count_show += count_add;
+    // });
     $('#show_more').click(function() {
         $.getJSON("/js/gameList.json", function(data) {
             for (var i = count_show; i < count_show + count_add; i++) {
