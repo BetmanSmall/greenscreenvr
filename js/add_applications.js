a4b0570d660717995df52b136e56b16cfcfd0b04
BetmanSmall <betmansmall@mail.ru>
2019-08-02 16:21:13 +0300

    small changes

--- js/add_applications.js
+++ js/add_applications.js
@@ -1,41 +1,75 @@
 
+var dataGames = [];
+var addedCount = 0;
+
 $(document).ready(function() {
-    $.getJSON("/apps_details.json", function(data) {
-        console.log(data);
-        if (data) {
-            for (var appIndex = 0; appIndex < data.length; appIndex++) {
-                var steam_appid = data[appIndex].steam_appid;
-                $('#gameList').append(
-                    '<div class="col-12 col-md-6 col-xl-4">' +
-                    '<div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
-                    '<div class="Portfolio wrapper">' +
-                    // '<a href="https://store.steampowered.com/app/' + steam_appid + '/">' +
-                    '<img class="card-img" src="' + data[appIndex].header_image + '">' +
-                    '<div id="slide">' +
-                    '<a class="game_info" href="#" data-toggle="modal" data-target="#m' + steam_appid + '"><img src="img/icons8-info-50.png" class="icon" alt=""></a>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>'
-                )
-                $('body').append(
-                    '<div class="modal fade bd-example-modal-lg" id="m' + steam_appid + '" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">' +
-                    '<div class="modal-dialog modal-lg modal-dialog-centered" role="document">' +
-                    '<div class="modal-content">' +
-                    '<div class="modal-header">' +
-                    '<h4 class="modal-title">' + data[appIndex].name + '</h4>' +
-                    '<button type="button" class="close" data-dismiss="modal">×</button>' +
-                    '</div>' +
-                    '<div class="modal-body">' +
-                    data[appIndex].short_description +
-                    '</div>' +
-                    '<div class="modal-footer">' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>'
-                )
-            }
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "apps_details.json",
+        'dataType': "json",
+        'success': function (data) {
+            dataGames = data;
         }
     });
+    $("#show_more").click();
 });
+
+function loadGames() {
+    return $.ajax({
+        type: "GET",
+        url: "/apps_details.json",
+        dataType: "json",
+        cache: false,
+        success: function(dataApp) {
+            console.log("Колличество игр: " + dataApp.length);
+        }
+    });
+}
+
+$('#show_more').click(function() {
+    var willGames = addedCount;
+    for (var i = 0; i < 6; i++) {
+        addGameCard(dataGames[willGames + i])
+    }
+    if (addedCount == dataGames.length) {
+        $('#show_more').remove();
+    }
+});
+
+function addGameCard(gameData) {
+    if (gameData) {
+        var steam_appid = gameData.steam_appid;
+        $('#gameList').append(
+            '<div class="col-12 col-md-6 col-xl-4">' +
+            '<div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
+            '<div class="Portfolio wrapper">' +
+            // '<a href="https://store.steampowered.com/app/' + steam_appid + '/">' +
+            '<img class="card-img" src="' + gameData.header_image + '">' +
+            '<div id="slide">' +
+            '<a class="game_info" href="#" data-toggle="modal" data-target="#m' + steam_appid + '"><img src="img/icons8-info-50.png" class="icon" alt=""></a>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>'
+        )
+        $('body').append(
+            '<div class="modal fade bd-example-modal-lg" id="m' + steam_appid + '" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">' +
+            '<div class="modal-dialog modal-lg modal-dialog-centered" role="document">' +
+            '<div class="modal-content">' +
+            '<div class="modal-header">' +
+            '<h4 class="modal-title">' + gameData.name + '</h4>' +
+            '<button type="button" class="close" data-dismiss="modal">×</button>' +
+            '</div>' +
+            '<div class="modal-body">' +
+            gameData.short_description +
+            '</div>' +
+            '<div class="modal-footer">' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>'
+        )
+        addedCount++;
+    }
+}
