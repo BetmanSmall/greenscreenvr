0ef116663688dfe21457f589bd8963c4adef32d0
BetmanSmall <betmansmall@mail.ru>
2019-10-11 22:47:39 +0300

    ie11 services update and other changes

--- js/add_applications.js
+++ js/add_applications.js
@@ -86,13 +86,15 @@ function addGameCard(gameData) {
         // console.log(steam_appid);
         // console.log("isNaN:" + isNaN(steam_appid));
         $('#gameList').append(
-            '<div class="col-12 col-md-6 col-xl-4">' +
-            '   <div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
-            '       <a class="game_info" href="#" data-toggle="modal" data-target="#m' + steam_appid + '">'+
+            // '<div class="col-12 col-md-6 col-xl-4">' +
+            // '   <div class="d-flex flex-column align-items-end justify-content-between p-3 block">' +
+            '<div class="col-lg-4 col-md-6">' +
+            '   <div class="p-3">' +
+            '       <a class="game_info" href="#" data-toggle="modal" data-target="#m' + steam_appid + '">' +
             '           <div class="Portfolio wrapper">' +
             '               <img class="card-img" src="' + gameData.header_image + '">' +
             '           </div>' +
-            '       </a>'+
+            '       </a>' +
             '   </div>' +
             '</div>'
         )
@@ -101,26 +103,26 @@ function addGameCard(gameData) {
             '  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">' +
             '    <div class="modal-content">' +
             '       <div class="modal-header">' +
-            '           <div class="divWithIcons" id="divWithIcons' + steam_appid + '"></div>'+
+            '           <div class="divWithIcons" id="divWithIcons' + steam_appid + '"></div>' +
             '           <h3 class="modal-title text-center" id="exampleModalCenterTitle">' + gameData.name + '</h3>' +
             '           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
             '       </div>' +
             '       <div class="modal-body">' +
             // '           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="false">'+
-            '           <div id="carouselInModal' + steam_appid + '" class="carousel slide carousel-fade" data-ride="carousel">'+
-            '               <div class="carousel-inner" role="listbox" id="cImDIV' + steam_appid + '"></div>'+
+            '           <div id="carouselInModal' + steam_appid + '" class="carousel slide carousel-fade" data-ride="carousel">' +
+            '               <div class="carousel-inner" role="listbox" id="cImDIV' + steam_appid + '"></div>' +
             '               <ol class="carousel-indicators" id="cImOL' + steam_appid + '"></ol>' +
-            '               <a class="carousel-controls carousel-control-prev" href="#carouselInModal' + steam_appid + '" role="button" data-slide="prev">'+
-            '                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>'+
-            '                   <span class="sr-only">Previous</span>'+
-            '               </a>'+
-            '               <a class="carousel-controls carousel-control-next" href="#carouselInModal' + steam_appid + '" role="button" data-slide="next">'+
-            '                   <span class="carousel-control-next-icon" aria-hidden="true"></span>'+
-            '                   <span class="sr-only">Next</span>'+
-            '               </a>'+
-            '           </div>'+
+            '               <a class="carousel-controls carousel-control-prev" href="#carouselInModal' + steam_appid + '" role="button" data-slide="prev">' +
+            '                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
+            '                   <span class="sr-only">Previous</span>' +
+            '               </a>' +
+            '               <a class="carousel-controls carousel-control-next" href="#carouselInModal' + steam_appid + '" role="button" data-slide="next">' +
+            '                   <span class="carousel-control-next-icon" aria-hidden="true"></span>' +
+            '                   <span class="sr-only">Next</span>' +
+            '               </a>' +
+            '           </div>' +
             '           <div class="modal-body-description">' +
-                            gameData.short_description +
+            gameData.short_description +
             '           </div>' +
             '       </div>' +
             '    </div>' +
@@ -130,15 +132,15 @@ function addGameCard(gameData) {
         // console.log(gameData.url);
         if (gameData.url) {
             $('#m' + steam_appid).find(".modal-content").append(
-                '<div class="modal-footer d-flex">' +
-                '    <button type="button" class="btn btn-primary m-auto" onclick="window.location.href=\'' + gameData.url + '\'">Go to</button>'+
+                '<div class="modal-footer">' +
+                '    <button type="button" class="btn btn-primary mx-auto" onclick="window.location.href=\'' + gameData.url + '\'">Go to</button>' +
                 // '    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'+
                 '</div>'
             )
         } else {
             $('#m' + steam_appid).find(".modal-content").append(
-                '<div class="modal-footer d-flex">' +
-                '    <button type="button" class="btn btn-primary m-auto" onclick="window.location.href=\'https://store.steampowered.com/app/' + steam_appid + '\'">Steam</button>'+
+                '<div class="modal-footer">' +
+                '    <button type="button" class="btn btn-primary mx-auto" onclick="window.location.href=\'https://store.steampowered.com/app/' + steam_appid + '\'">Steam</button>' +
                 // '    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'+
                 '</div>'
             )
@@ -155,11 +157,11 @@ function addGameCard(gameData) {
         }
         addedCount++;
 
-        $('#carouselInModal'+steam_appid).carousel({
+        $('#carouselInModal' + steam_appid).carousel({
             touch: true,
             interval: false
         })
-        $('#carouselInModal'+steam_appid).on('slide.bs.carousel', function (e) {
+        $('#carouselInModal' + steam_appid).on('slide.bs.carousel', function(e) {
             var iframes = e.target.querySelectorAll("iframe");
             // console.log(iframes);
             // console.log(iframes.length);
@@ -204,21 +206,26 @@ function addGameCard(gameData) {
             }
         });
 
-        $("#m"+steam_appid).on('hidden.bs.modal', function (e) {
+        $("#m" + steam_appid).on('hidden.bs.modal', function(e) {
             clearInterval(timerId);
             var videos = document.getElementById(e.target.getAttribute('id')).querySelectorAll("video");
-            videos.forEach(function(v) {
-                v.pause();
-            });
+            // videos.forEach(function(v) {
+            //     v.pause();
+            // });
+            for (var i = 0; i < videos.length; i++) {
+                videos[i].pause();
+            }
             var iframes = document.getElementById(e.target.getAttribute('id')).querySelectorAll("iframe");
             // console.log(iframes);
-            iframes.forEach(function(v) {
-                v.src = v.src;
-                // v.pauseVideo();
-            });
+            // iframes.forEach(function(v) {
+            //     v.src = v.src;
+            // });
+            for (var i = 0; i < iframes.length; i++) {
+                iframes[i].src = iframes[i].src;
+            }
         });
 
-        $("#m"+steam_appid).on('shown.bs.modal', function (e) {
+        $("#m" + steam_appid).on('shown.bs.modal', function(e) {
             var carouselInner = e.target.querySelector(".carousel-inner");
             if (carouselInner.childElementCount == 0) {
                 // console.log("#m"+steam_appid + " init!");
@@ -226,8 +233,8 @@ function addGameCard(gameData) {
                     // console.log("gameData.movies[m].youtube_url:" + gameData.movies[m].youtube_url);
                     if (!gameData.movies[m].youtube_url) {
                         $('#cImDIV' + steam_appid).append(
-                            '<div class="carousel-item' + ((m==0) ? (' active'):'') + '\">'+
-                            '    <video id="video_'+m+'_'+steam_appid+'" class="d-block video-fluid w-100" controls playsinline poster="' + gameData.movies[m].thumbnail + '">' +
+                            '<div class="carousel-item' + ((m == 0) ? (' active') : '') + '\">' +
+                            '    <video id="video_' + m + '_' + steam_appid + '" class="d-block video-fluid w-100" controls playsinline poster="' + gameData.movies[m].thumbnail + '">' +
                             '        <source src="' + gameData.movies[m].webm.low + '.mp4" type="video/mp4" />' +
                             // '        <source src="' + gameData.movies[m].webm.max + '.mp4" type="video/mp4" data-quality="hd"/>' +
                             '        <source src="' + gameData.movies[m].webm.low + '.webm" type="video/webm" />' +
@@ -237,35 +244,34 @@ function addGameCard(gameData) {
                         )
                     } else {
                         $('#cImDIV' + steam_appid).append(
-                            '<div class="carousel-item' + ((m==0) ? (' active'):'') + '\">'+
-                            '   <div class="carousel-video-inner">'+
-                            '       <iframe id="ytplayer" type="text/html" width="100%" height="" src="https://www.youtube.com/embed/' + gameData.movies[m].youtube_url + '" frameborder="0"/>'+
+                            '<div class="carousel-item' + ((m == 0) ? (' active') : '') + '\">' +
+                            '   <div class="carousel-video-inner">' +
+                            '       <iframe id="ytplayer" type="text/html" width="100%" height="" src="https://www.youtube.com/embed/' + gameData.movies[m].youtube_url + '" frameborder="0"/>' +
                             // '       <div class="video-player" id="video-player_'+m+'_'+steam_appid+'" data-video-id="' + gameData.movies[m].youtube_url + '"></div>'+
-                            '   </div>'+
+                            '   </div>' +
                             '</div>'
                         )
                     }
                     $('#cImOL' + steam_appid).append(
-                        '<li data-target="#carouselInModal' + steam_appid + '" data-slide-to="'+(m)+'"' + ((m==0)?('class=\"active'):'') + '\"></li>'
+                        '<li data-target="#carouselInModal' + steam_appid + '" data-slide-to="' + (m) + '"' + ((m == 0) ? ('class=\"active') : '') + '\"></li>'
                     )
                 }
                 for (var s = 0; s < gameData.screenshots.length; s++) {
                     $('#cImDIV' + steam_appid).append(
-                        '<div class="carousel-item' + ((gameData.movies.length==0) && (s==0) ? (' active'):'') + '\">'+
-                        '    <img class="d-block w-100" src="' + gameData.screenshots[s].path_thumbnail + '">'+
+                        '<div class="carousel-item' + ((gameData.movies.length == 0) && (s == 0) ? (' active') : '') + '\">' +
+                        '    <img class="d-block w-100" src="' + gameData.screenshots[s].path_thumbnail + '">' +
                         '</div>'
                     )
                     $('#cImOL' + steam_appid).append(
-                        '<li data-target="#carouselInModal' + steam_appid + '" data-slide-to="'+(gameData.movies.length+s)+'"></li>'
+                        '<li data-target="#carouselInModal' + steam_appid + '" data-slide-to="' + (gameData.movies.length + s) + '"></li>'
                     )
                 }
-                var videos = document.getElementById('carouselInModal'+steam_appid).querySelectorAll("video");
+                var videos = document.getElementById('carouselInModal' + steam_appid).querySelectorAll("video");
                 videos.forEach(function(e) {
                     e.addEventListener('ended', videoEnded, false);
                     e.addEventListener('pause', videoPause, false);
                     e.addEventListener('play', videoPlay, false);
                     e.addEventListener('volumechange', videoVolumechange, false);
-                    // createVideo(e);
                 });
             }
             var carouselItemActive = e.target.querySelector(".carousel-item.active");
@@ -285,12 +291,12 @@ function addGameCard(gameData) {
 
 function carouselNextSlide(carouselID) {
     console.log("carouselNextSlide " + carouselID);
-    $("#"+carouselID).carousel('next');
+    $("#" + carouselID).carousel('next');
 }
 
 function videoEnded(e) {
     var carouselID = e.path[3].getAttribute("id");
-    $("#"+carouselID).carousel('next');
+    $("#" + carouselID).carousel('next');
     // $('#'+carouselID).carousel();
     // console.log("in " + carouselID + " videoEnded!:");
     // console.log(e);
@@ -317,62 +323,3 @@ function videoVolumechange(e) {
     // console.log("videoVolumechange!:" + globalVolume);
     // console.log(e);
 }
-
-// $(function () {
-//     $(".carousel").on("slide.bs.carousel", function (e) {
-//         console.log("carousel(slide.bs.carousel); e:" + e);
-//         console.log(e);
-//         var prev = $(this)
-//             .find(".active")
-//             .index();
-//         console.log(prev);
-//         var next = $(e.relatedTarget).index();
-//         console.log(next);
-//         var video = $(".video-player")[0];
-//         console.log(video);
-//         var videoSlide = $(".video-player")
-//             .closest(".carousel-item")
-//             .index();
-//         if (next === videoSlide) {
-//             if (video.tagName == "IFRAME") {
-//                 player.playVideo();
-//             } else {
-//                 createVideo(video);
-//             }
-//         } else {
-//             if (typeof player !== "undefined") {
-//                 player.pauseVideo();
-//             }
-//         }
-//     });
-// });
-
-// function createVideo(video) {
-//     console.log("createVideo(); video:" + video);
-//     var youtubeScriptId = "youtube-api";
-//     var youtubeScript = document.getElementById(youtubeScriptId);
-//     console.log("createVideo(); youtubeScript:");
-//     console.log(youtubeScript);
-//     var videoId = video.getAttribute("data-video-id");
-//     console.log(videoId);
-
-//     if (youtubeScript === null) {
-//         var tag = document.createElement("script");
-//         var firstScript = document.getElementsByTagName("script")[0];
-
-//         tag.src = "https://www.youtube.com/iframe_api";
-//         tag.id = youtubeScriptId;
-//         firstScript.parentNode.insertBefore(tag, firstScript);
-//     }
-
-//     window.onYouTubeIframeAPIReady = function () {
-//         window.player = new window.YT.Player(video, {
-//             videoId: videoId,
-//             playerVars: {
-//                 autoplay: 1,
-//                 modestbranding: 1,
-//                 rel: 0
-//             }
-//         });
-//     };
-// }
